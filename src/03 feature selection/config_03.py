f2ca9c3becc4f348dba624b8d67f90b5aa6326d4
FoekeBoersma <foekeboersma@hotmail.com>
2024-08-04 18:46:57 +0200

    location management 03 cv-aic-mse-r2

--- src/03 feature selection/config_03.py
+++ src/03 feature selection/config_03.py
@@ -1,4 +1,8 @@
-dataset = {
+input_dataset = {
     "globaldataset": "data/input/GlobalModelData/ModellingDataset-Global.csv",
     "medianshapcv10": "data/input/GlobalModelData/df median shap cv10.csv"
+}
+
+output = {
+    "output_map": "/data/output/07 evaluating"
 }
