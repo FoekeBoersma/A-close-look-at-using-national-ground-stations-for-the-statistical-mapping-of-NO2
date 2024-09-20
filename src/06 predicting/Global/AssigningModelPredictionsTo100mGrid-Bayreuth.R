b861cc1802bc41bb7c07f6f8e3cb41dbde7790d8
FoekeBoersma <foekeboersma@hotmail.com>
2024-09-20 18:39:12 +0200

    location management - editing paths (06)

--- src/06 predicting/Global/AssigningModelPredictionsTo100mGrid-Bayreuth.R
+++ src/06 predicting/Global/AssigningModelPredictionsTo100mGrid-Bayreuth.R
@@ -15,7 +15,7 @@ library(dplyr)
 #IMPORT GEODATA
 
 #import area of interest at 100m resolution
-grid <- readOGR('C:/Users/foeke/OneDrive/Documenten/submitting paper/TooBigData/grid100Bayreuth.gpkg')
+grid <- readOGR('/TooBigData/grid100Bayreuth.gpkg')
 
 ## == data processing == ##
 
@@ -23,11 +23,11 @@ grid <- readOGR('C:/Users/foeke/OneDrive/Documenten/submitting paper/TooBigData/
 grid_sf <- st_as_sf(grid)
 
 #Modelpredictions
-modelPredictions <- read.csv('C:/Users/foeke/OneDrive/Documenten/submitting paper/TooBigData/SpatialPredictionPatterns/Predicting NO2-AllModelsBayreuth100_xy.csv')
+modelPredictions <- read.csv('/TooBigData/SpatialPredictionPatterns/Predicting NO2-AllModelsBayreuth100_xy.csv')
 modelPredictions_sf <- st_as_sf(modelPredictions, coords=c("Longitude", "Latitude"), crs=4326)
 #make crs similar
 modelPredictions_sf <- st_transform(modelPredictions_sf, crs=st_crs(grid_sf))
 Bayreuth_NO2PredictionPerModel <- st_join(grid_sf, modelPredictions_sf, join = st_nearest_feature)
 
 #export option
-sf::st_write(Bayreuth_NO2PredictionPerModel, dsn='C:/Users/foeke/OneDrive/Documenten/submitting paper/TooBigData/Grid100/Bayreuth_NO2PredictionPerModel.gpkg', driver = "GPKG")
+sf::st_write(Bayreuth_NO2PredictionPerModel, dsn='/TooBigData/Grid100/Bayreuth_NO2PredictionPerModel.gpkg', driver = "GPKG")
--- src/06 predicting/Global/AssigningModelPredictionsTo100mGrid-Utrecht.R
+++ src/06 predicting/Global/AssigningModelPredictionsTo100mGrid-Utrecht.R
@@ -16,7 +16,7 @@ library(dplyr)
 #IMPORT GEODATA
 
 #import area of interest at 100m resolution
-grid <- readOGR('C:/Users/foeke/OneDrive/Documenten/submitting paper/TooBigData/grid100Utrecht.gpkg')
+grid <- readOGR('/TooBigData/grid100Utrecht.gpkg')
 
 ## == data processing == ##
 
@@ -24,11 +24,11 @@ grid <- readOGR('C:/Users/foeke/OneDrive/Documenten/submitting paper/TooBigData/
 grid_sf <- st_as_sf(grid)
 
 #Modelpredictions
-modelPredictions <- read.csv('C:/Users/foeke/OneDrive/Documenten/submitting paper/TooBigData/SpatialPredictionPatterns/Predicting NO2-AllModelsUtrecht100_xy.csv')
+modelPredictions <- read.csv('/TooBigData/SpatialPredictionPatterns/Predicting NO2-AllModelsUtrecht100_xy.csv')
 modelPredictions_sf <- st_as_sf(modelPredictions, coords=c("Longitude", "Latitude"), crs=4326)
 #make crs similar
 modelPredictions_sf <- st_transform(modelPredictions_sf, crs=st_crs(grid_sf))
 Hamburg_NO2PredictionPerModel <- st_join(grid_sf, modelPredictions_sf, join = st_nearest_feature)
 
 #export option
-sf::st_write(Hamburg_NO2PredictionPerModel, dsn='C:/Users/foeke/OneDrive/Documenten/submitting paper/TooBigData/Grid100/Utrecht_NO2PredictionPerModel.gpkg', driver = "GPKG")
+sf::st_write(Hamburg_NO2PredictionPerModel, dsn='/TooBigData/Grid100/Utrecht_NO2PredictionPerModel.gpkg', driver = "GPKG")
