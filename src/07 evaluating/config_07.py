1c20ebc2b23c6cf8a20007236f4b627302560859
FoekeBoersma <foekeboersma@hotmail.com>
2024-09-29 14:36:00 +0200

    comparing-predicted-no2-values-cities-models: from ipynb to py

---
+++ src/07 evaluating/config_07.py
@@ -0,0 +1,16 @@
+
+
+input_data = {
+'amsterdam_predicting_no2_allmodels' : 'data/input/TooBigData/Amsterdam/Predicting NO2-AllModelsAmsterdam100_xy.csv',
+
+'bayreuth_predicting_no2_allmodels': 'data/input/TooBigData/Bayreuth/Predicting NO2-AllModelsBayreuth100_xy.csv',
+
+'hamburg_predicting_no2_allmodels': 'data/input/TooBigData/Hamburg/Predicting NO2-AllModelsHamburg100_xy.csv',
+
+'utrecht_predicting_no2_allmodels' : 'data/input/TooBigData/Utrecht/Predicting NO2-AllModelsUtrecht100_xy.csv'
+} 
+
+
+output = {
+    "output_map": "data/output/07 evaluating"
+}
