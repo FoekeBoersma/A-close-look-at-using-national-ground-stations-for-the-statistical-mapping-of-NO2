c45d4c60640943fd14bc4562aee5ddd3b7db78d8
FoekeBoersma <foekeboersma@hotmail.com>
2024-11-10 18:13:23 +0100

    location management + ipynb to py

---
+++ src/06 predicting/config_06.py
@@ -0,0 +1,16 @@
+
+
+input_data = {
+'predicting_dataset' : 'data/input/GlobalModelData/PredictingDataset.csv',
+'grid100_amsterdam_csv': 'data/input/TooBigData/Amsterdam/grid100_GlobalPredictors-Amsterdam.csv',
+'grid100_bayreuth_csv':'data/input/TooBigData/Bayreuth/grid100_GlobalPredictors-Bayreuth.csv',
+'grid100_hamburg_csv':'data/input/TooBigData/Hamburg/grid100_GlobalPredictors-HH.csv',
+'grid100_utrecht_csv':'data/input/TooBigData/Utrecht/grid100_GlobalPredictors-Utrecht.csv'
+
+
+}
+
+output = {
+    "output_map": "data/output/07 evaluating",
+    "output_map_excel": "data/output/07 evaluating/excel"
+}
