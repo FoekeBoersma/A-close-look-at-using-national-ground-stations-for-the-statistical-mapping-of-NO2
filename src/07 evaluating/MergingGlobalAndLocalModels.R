4ba20335a62e24720d6e513dd5c8d0f397fb22d1
FoekeBoersma <foekeboersma@hotmail.com>
2024-11-10 17:37:43 +0100

    location management

--- src/07 evaluating/MergingGlobalAndLocalModels.R
+++ src/07 evaluating/MergingGlobalAndLocalModels.R
@@ -5,10 +5,36 @@ library(purrr)
 library(sf)
 library(raster)
 library("writexl")
+library(yaml)
+
+# Connect to YAML file
+current_dir <- rstudioapi::getActiveDocumentContext()$path
+config_dir <- dirname(dirname(current_dir)) # One level up in directory
+config07_path <- file.path(config_dir, "config_07.yml")
+
+# Read YAML configuration file
+config07 <- yaml::yaml.load_file(config07_path)
+# Define the parent directory (move four levels up)
+parent_directory <- dirname(dirname(dirname(dirname(current_dir))))
+
+# Paths for input data based on YAML configuration
+Amsterdam_NO2PredictionPerModel_dir <- normalizePath(file.path(parent_directory, config07$input_data$Amsterdam_NO2PredictionPerModel), winslash = "/")
 
 
-#global
-global = readOGR('C:/Users/foeke/OneDrive/Documenten/submitting paper/TooBigData/Grid100/Amsterdam_NO2PredictionPerModel.gpkg')
+# Import global dataset as an sf object, projected onto Amsterdam (if shapefile format is used)
+global <- st_read(Amsterdam_NO2PredictionPerModel_dir)
+
+global <- global %>%
+  rename(
+    predicted_NO2_RF = p_NO2_RF,
+    predicted_NO2_LASSO = p_NO2_LA,
+    predicted_NO2_RIDGE = p_NO2_RI,
+    predicted_NO2_LightGBM = p_NO2_LG,
+    predicted_NO2_XGBoost = p_NO2_X
+  )
+
+# Define output directory
+out_location_dir <- normalizePath(file.path(parent_directory, config07$out_location), winslash = "/")
 
 global_df <- as.data.frame(global)
 
@@ -27,12 +53,20 @@ for(model in global_models){
 }
 
 #local
-linear = readOGR("C:/Users/foeke/OneDrive/Documenten/submitting paper/TooBigData/LocalModels/predictedNO2_Linear.gpkg")
-linear_separated = readOGR("C:/Users/foeke/OneDrive/Documenten/submitting paper/TooBigData/LocalModels/predictedNO2_Linear_SeparatingSpatialGroups.gpkg")
-MEM = readOGR("C:/Users/foeke/OneDrive/Documenten/submitting paper/TooBigData/LocalModels/predictedNO2_MEM.gpkg")
-UK = readOGR("C:/Users/foeke/OneDrive/Documenten/submitting paper/TooBigData/LocalModels/predictedNO2_UK_formula.gpkg")
-UK_separated = readOGR("C:/Users/foeke/OneDrive/Documenten/submitting paper/TooBigData/LocalModels/predictedNO2_UK_SeparatingSpatialGroups.gpkg")
-OK = readOGR("C:/Users/foeke/OneDrive/Documenten/submitting paper/TooBigData/LocalModels/predictedNO2_OK.gpkg")
+predictedNO2_Linear_dir <- normalizePath(file.path(parent_directory, config07$input_data$predictedNO2_Linear), winslash = "/")
+predictedNO2_Linear_SeparatingSpatialGroups_dir <- normalizePath(file.path(parent_directory, config07$input_data$predictedNO2_Linear_SeparatingSpatialGroups), winslash = "/")
+predictedNO2_MEM_dir <- normalizePath(file.path(parent_directory, config07$input_data$predictedNO2_MEM), winslash = "/")
+predictedNO2_UK_formula_dir <- normalizePath(file.path(parent_directory, config07$input_data$predictedNO2_UK_formula), winslash = "/")
+predictedNO2_UK_SeparatingSpatialGroups_dir <- normalizePath(file.path(parent_directory, config07$input_data$predictedNO2_UK_SeparatingSpatialGroups), winslash = "/")
+predictedNO2_OK_dir <- normalizePath(file.path(parent_directory, config07$input_data$predictedNO2_OK), winslash = "/")
+
+
+linear = readOGR(predictedNO2_Linear_dir)
+linear_separated = readOGR(predictedNO2_Linear_SeparatingSpatialGroups_dir)
+MEM = readOGR(predictedNO2_MEM_dir)
+UK = readOGR(predictedNO2_UK_formula_dir)
+UK_separated = readOGR(predictedNO2_UK_SeparatingSpatialGroups_dir)
+OK = readOGR(predictedNO2_OK_dir)
 
 #make spatial - obtain the geometry for each sample
 grid100_sf <- st_as_sf(linear)
@@ -46,27 +80,16 @@ UK_df <- as.data.frame(UK)
 UK_separated_df <- as.data.frame(UK_separated)
 OK_df <- as.data.frame(OK)
 
-
 colnames(linear_df)
-
 colnames(linear_separated_df)
-
 colnames(MEM_df)
-
 colnames(UK_df)
-
 colnames(UK_separated_df)
-
 colnames(OK_df)
 
 #merge local dataframes to one dataframe, containing all predictions 
 merge_local = list(linear_df, linear_separated_df, MEM_df, UK_df, UK_separated_df, OK_df)
-
-
 merge_local <- merge_local %>% reduce(full_join, by= 'key')
-
-merge_local
-
 
 #rename
 merge_local <- merge_local %>% rename("nightlight_450" = "nightlight_450.x",
@@ -92,17 +115,14 @@ local <- merge_local[,c("nightlight_450" ,"nightlight_4950" ,"population_1000","
 
 local
 local_sf <- merge(geo, local, by="key")
-
 local_df <- as.data.frame(local)
 
 
-
-
 #to excel
-write_xlsx(local_df, "C:/Users/foeke/OneDrive/Documenten/submitting paper/TooBigData/AllModels/Local_df.xlsx")
+write_xlsx(local_df, file.path(out_location_dir,"Local_df.xlsx"))
 
 ## == export option == ##
-#sf::st_write(local_sf, dsn="C:/Users/foeke/OneDrive/Documenten/submitting paper/TooBigData/AllModels/local_predictions.gpkg", driver = "GPKG")
+#sf::st_write(local_sf, dsn=file.path(out_location_dir,"local_predictions.gpkg"), driver = "GPKG")
 
 ## == assign NO2 tif data to local models == ##
 
@@ -110,7 +130,10 @@ write_xlsx(local_df, "C:/Users/foeke/OneDrive/Documenten/submitting paper/TooBig
 local_cen <- st_centroid(local_sf,byid=TRUE)
 
 #NO2tif
-NO2tif <- ('C:/Users/foeke/OneDrive/Documenten/submitting paper/All scripts - paper/data/no2_Amsterdam/NO2.tif')
+
+no2tif_dir <- normalizePath(file.path(parent_directory, config07$input_data$no2tif), winslash = "/")
+
+NO2tif <- (no2tif_dir)
 NO2tif=raster(NO2tif)
 
 #plot(NO2tif)
@@ -127,10 +150,10 @@ View(local_and_no2tif_grid)
 local_and_no2tif_grid = subset(local_and_no2tif_grid, select = -c(coords.x1,coords.x2) )
 
 ## == export option == ##
-sf::st_write(local_and_no2tif_grid, dsn="C:/Users/foeke/OneDrive/Documenten/submitting paper/TooBigData/AllModels/local_and_no2tif_grid.gpkg", driver = "GPKG")
+sf::st_write(local_and_no2tif_grid, dsn=file.path(out_location_dir,"local_and_no2tif_grid.gpkg"), driver = "GPKG")
 
 global_sf <- st_as_sf(global)
 all_models <- st_join(local_and_no2tif_grid, global_sf, largest = T, left = T)
 
 ## == export option == ##
-sf::st_write(all_models, dsn="C:/Users/foeke/OneDrive/Documenten/submitting paper/TooBigData/AllModels/all_models.gpkg", driver = "GPKG")
+sf::st_write(all_models, dsn=file.path(out_location_dir,"all_models.gpkg"), driver = "GPKG")
