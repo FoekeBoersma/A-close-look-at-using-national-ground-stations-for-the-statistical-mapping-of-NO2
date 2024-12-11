74e9c4cf8f7c4df3a2e30a3dcdc4b4a9835236f4
FoekeBoersma <foekeboersma@hotmail.com>
2024-12-11 22:22:39 +0100

    chore: remove lightgbm in script and add map output

--- src/07 evaluating/Global/cross-validation-model-performances-global.py
+++ src/07 evaluating/Global/cross-validation-model-performances-global.py
@@ -4,7 +4,6 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 import xgboost as xgb
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_absolute_error
 from sklearn.linear_model import Ridge, RidgeCV, Lasso, LassoCV
@@ -18,14 +17,16 @@ import sys
 # Get the directory of the current script
 current_directory = os.path.dirname(os.path.abspath(__file__)) 
 config_directory = os.path.abspath(os.path.join(current_directory, '..'))
+root_directory = os.path.abspath(os.path.join(current_directory, '../../..'))
 sys.path.append(config_directory)
+sys.path.append(root_directory)
 
 # Try importing the config_07 module
 try:
     import config_07
 except ModuleNotFoundError as e:
     print(f"Error importing module: {e}")
-from functions import cross_validate_models
+from functions import cross_validate_models, create_new_map
 predicting_dataset = config_07.input_data['predicting_dataset']
 
 n_estimators_random_forest = config_07.parameters_random_forest ['n_estimators_random_forest']
@@ -54,7 +55,11 @@ learning_rate_xgboost = config_07.parameters_xgboost['learning_rate_xgboost']
 n_estimators_xgboost = config_07.parameters_xgboost['n_estimators_xgboost']
 random_state_xgboost = config_07.parameters_xgboost['random_state_xgboost']
 
-output_map = config_07.output['output_map']
+output_map = root_directory + '/' + config_07.output['output_map']
+output_map_global_model_performance = config_07.output['output_map_global_model_performance']
+create_new_map(output_map_global_model_performance, output_map)
+
+output_map = output_map + '/' + output_map_global_model_performance + '/'
 
 df = pd.read_csv(predicting_dataset, sep=',')
 #remove geodata
@@ -105,11 +110,12 @@ model_lasso = Lasso(alpha=0.1)
 ridge2 = Ridge(alpha = 0.3)
 #random states - CV = 20
 random_states = [1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95]
+
 #random state is needed to ensure that same results are generated each time.
 #models
-models = [rf, lightgbm_model, xgb, model_lasso, ridge2]
-#model_names
-model_names = ['rf', 'lgb', 'xgb', 'lasso', 'ridge']
+models = [rf, xgb, model_lasso, ridge2]
+# #model_names
+model_names = ['rf', 'xgb', 'lasso', 'ridge']
 
 #create dictionaries that eventually stores performance per model (CV)
 total_rmse = {}
@@ -130,7 +136,7 @@ df_total_mae = pd.DataFrame(total_mae)
 # RMSE Plot 
 plt.rcParams["font.family"] = "serif"
 fig, ax = plt.subplots()
-fig.suptitle('Variance in Root Mean Square Error (CV=20) per model', fontsize=12, fontweight='bold')
+fig.suptitle(f'Variability in Root Mean Square Error (CV={len(random_states)}) per model', fontsize=12, fontweight='bold')
 
 # Use boxplot on DataFrame 
 ax.boxplot([df_total_rmse[model] for model in df_total_rmse.columns])
@@ -143,12 +149,12 @@ ax.set_ylabel('RMSE')
 
 plot_filename = os.path.join(output_map, 'RMSE - CV20 - ModelPerformances')
 plt.savefig(plot_filename, dpi=100)
-plt.show()
+
 
 # R2 Plot 
 plt.rcParams["font.family"] = "serif"
 fig, ax = plt.subplots()
-fig.suptitle('Variance in R2 (CV=20) per model', fontsize=14, fontweight='bold')
+fig.suptitle(f'Variability in R2 (CV={len(random_states)}) per model', fontsize=14, fontweight='bold')
 
 # Use boxplot on each model’s R2 values 
 ax.boxplot([df_total_r2[model] for model in df_total_r2.columns])
@@ -161,12 +167,11 @@ ax.set_ylabel('R2')
 
 plot_filename = os.path.join(output_map, 'R2 - CV20 - ModelPerformances')
 plt.savefig(plot_filename, dpi=100)
-plt.show()
 
 # MAE Plot 
 plt.rcParams["font.family"] = "serif"
 fig, ax = plt.subplots()
-fig.suptitle('Variance in MAE (CV=20) per model', fontsize=14, fontweight='bold')
+fig.suptitle(f'Variability in MAE (CV={len(random_states)}) per model', fontsize=14, fontweight='bold')
 
 # Use boxplot on each model’s MAE values 
 ax.boxplot([df_total_mae[model] for model in df_total_mae.columns])
@@ -179,5 +184,5 @@ ax.set_ylabel('MAE')
 
 plot_filename = os.path.join(output_map, 'MAE - CV20 - ModelPerformances')
 plt.savefig(plot_filename, dpi=100)
-plt.show()
+
 
